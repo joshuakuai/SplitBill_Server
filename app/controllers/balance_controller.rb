ea54233a45e915572373e885d16be491a718ed3e
joshuakuai <bearworldbrave@gmail.com>
2015-03-07 10:57:24 -0500

    Update payment controller

--- app/controllers/balance_controller.rb
+++ app/controllers/balance_controller.rb
@@ -1,97 +1,114 @@
 require 'stripe'
 
 class BalanceController < ApplicationController
-    rescue_from ActionController::ParameterMissing do
+  before_action :get_user
+  
+  Stripe.api_key = "sk_test_HjgBPaq7P9RBbtyKejj3wwP7"
+  
+  rescue_from ActionController::ParameterMissing do
     render :msg => "Invalid Request.", :status => 400
+  end
+    
+  #POST /deposite
+  def deposite
+      params.permit(:stripe_token, :amount)
+
+      if params[:amount] < 0
+        render json: { error: "Invalid amount."}, status: 406 and return
+      end
+      
+      begin
+        Stripe::Charge.create(
+          :amount => params[:amount],
+          :currency => "usd",
+          :source => params[:stripe_token]
+          :description => "Deposit for #{@user.email}"
+        )
+      rescue Stripe::CardError => e
+        # The card has been declined
+        body = e.json_body
+        err  = body[:error]
+        render json: { error: "Failed to charge, #{err[:message]}"}, status: 406 and return
+      rescue Stripe::APIConnectionError => e
+        # Network communication with Stripe failed
+        render json: { error: "The payment system is not avaiable at this time."}, status: 500 and return
+      end
+      
+      @user.balance += params[:amount]
+      @user.save
+      render json: {}, status: 200
+  end
+
+  #POST /transfer
+  def transfer
+      params.permit(:target_user, :amount)
+
+      target_user = User.where(email: params[:target_user]).first
+
+      #check the balance
+      if user.balance >= params[:amount]
+          if target_user
+              target_user.balance += params[:amount]
+              target_user.save
+              
+              @user.balance -= params[:amount]
+              @user.save
+          else
+              render json: { error: "Target account doesn't exist."}, status: 406
+          end
+      else
+          render json: { error: "Your balance is insufficient." }, status: 402
+      end
+  end
+
+  #POST /withdraw
+  def withdraw
+      params.permit(:full_name, :amount, :bank_email, :bank_account)
+  
+      #check the balance
+      if user.balance >= params[:amount]          
+          begin
+            user_recipient = Stripe::Recipient.create(
+                            :name => params[:full_name],
+                            :type => "individual",
+                            :bank_account => params[:bank_account],
+                            :email => params[:bank_email]
+                            )
+            
+            Stripe::Transfer.create(
+              :amount => params[:amount],
+              :currency => "usd",
+              :recipient => user_recipient,
+              :bank_account => params[:bank_account],
+              :statement_descriptor => "Withdraw for #{@user.email}"
+            )
+          rescue Stripe::CardError => e
+            # The card has been declined
+            body = e.json_body
+            err  = body[:error]
+            render json: { error: "Failed to charge, #{err[:message]}"}, status: 406 and return
+          rescue Stripe::APIConnectionError => e
+            # Network communication with Stripe failed
+            render json: { error: "The payment system is not avaiable at this time."}, status: 500 and return
+          end
+          
+          @user.balance -= params[:amount]
+          @user.save
+          render json: {}, status: 200
+      else
+          render json: { error: "Your balance is insufficient." }, status: 402
+      end
+  end
+  
+  private
+  
+  # Make sure this request comes from valid user
+  def get_user
+    params.require(:id).require(:token)
+    @user = User.where(idUser: params[:id]).first
+    if !(@user && (@user.session_token == params[:token]))
+      render json: { error: "Account doesn't exist." }, status: 401
+    end
+  end
 end
 
-#POST /deposite
-def deposite
-    params.permit(:id, :token, :stripe_token, :amount)
-    user=User.where(idUser: params[:id]).first
-    
-    #check the existance of account
-    if user
-        user_charge=Stripe::Charge.create(
-                              :amount => params[:amount],
-                              :currency => "usd",
-                              :source => params[:stripe_token]
-                              )
-        if user_charge
-            user.change_balance(params[:amount])
-        else
-            render json: {
-                error: "Invalid stripe token.",
-            }, status: 406
-        end
-    else
-        render json: {
-            error: "Account doesn't exist.",
-        }, status: 401
-    end
-end
-
-#POST /transfer
-def transfer
-    params.permit(:id, :token, :target_user, :amount)
-    user=User.where(idUser: params[:id]).first
-    target_user=User.where(email: params[:target_user]).first
-    
-    #check the existance of account
-    #check the balance
-    if user
-        if user.balance>=params[:amount]
-            if target_user
-                target_user.change_balance(amount)
-                target_user.save
-                user.change_balance(-amount)
-                user.save
-            else
-                render json: {
-                    error: "Target account doesn't exist.",
-                }, status: 406
-            end
-        else
-            render json: {
-                error: "Your balance is insufficient.",
-            }, status: 402
-        end
-    else
-        render json: {
-            error: "Account doesn't exist.",
-        }, status: 401
-    end
-end
-
-#POST /withdraw
-def withdraw
-    params.permit(:id, :token, :full_name, :amount, :bank_email, :stripe_token)
-    user=User.where(idUser: params[:id]).first
-    
-    #check the existance of account
-    #check the balance
-    if user
-        if user.balance>=params[:amount]
-            user_recipient=Stripe::Recipient.create(
-                                                    :name => params[:full_name],
-                                                    :type => "individual",
-                                                    :card => params[:stripe_token]
-                                                    )
-            if user_recipient
-                user.change_balance(-amount)
-            else
-                render json: {
-                    error: "Invalid stripe token.",
-                }, status: 406
-            end
-        else
-            render json: {
-                error: "Your balance is insufficient.",
-            }, status: 402
-        end
-    else
-        render json: {
-            error: "Account doesn't exist.",
-        }, status: 401
-    end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
   has_many :authen_codes
-  def change_balance
-      params.permit(:amount)
-      @balance+=params[:amount]
-  end
+  validates :balance, numericality: { greater_than: 0 }
 end
