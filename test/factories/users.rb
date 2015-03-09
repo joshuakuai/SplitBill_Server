66a4beeb554ad720995d65b3485db015d07b8226
joshuakuai <bearworldbrave@gmail.com>
2015-03-09 11:19:42 -0400

    Tweak the test database

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,33 +1,33 @@
 FactoryGirl.define do 
   factory :user do
-    id 1
+    id 2
     email "bearworldbrave@gmail.com"
     password "123456"
     session_token "texttoken"
     balance 100
     
     factory :invalid_email_user do
-      id 2
+      id 3
       email "asdasdjklsdajkl"
     end
     
     factory :empty_password_user do
-      id 3
+      id 4
       password nil
     end
     
     factory :empty_authen_code_user do
-      id 4
+      id 5
       session_token nil
     end
     
     factory :negative_balance_user do
-      id 5
+      id 6
       balance -100
     end
     
     factory :transfer_target_user do
-      id 6
+      id 7
       email "transfer@gmail.com"
     end
   end
